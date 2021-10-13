aa7c4e385457d2541a56f97ccce0e30eea3202b9
Chirag Lulla <lullachirag239@gmail.coom>
2021-10-13 19:13:09 +0530

    Add initial markdown but preview not working

--- src/App.js
+++ src/App.js
@@ -5,8 +5,33 @@ import Preview from "./Preview";
 
 function App() {
 
-  const [ text, setText ] = useState("")
-  const [ preview, setPreview ] = useState("")
+  let initialText = `
+    # This is the biggest text you can ever write.
+    ## This is the second biggest text you can write.
+    ###### This is the smallest text you can write.
+    
+    ### **Grab the reader's attention with some Bold text.**
+    ### *Love for italic text never ends.*
+    ### ~~Strikethrough the text you wish to.~~
+    
+    ### **You can also _nest_ italic inside bold text.**
+    
+    ### ***Italic and bold at the same time.***
+    
+    Normal Text
+    > Quoted Text
+    
+    ### \`Add text which look like real <code></code> as in your editor.\` 
+    
+    \`\`\`
+      const Editor = () => {
+          return "Convert me to some code looking text.";
+      }
+    \`\`\`
+  `
+
+  const [ text, setText ] = useState(initialText)
+  const [ preview, setPreview ] = useState(marked(initialText))
  
   const handleChange = e => {
     const text = e.target.value
@@ -19,7 +44,6 @@ function App() {
       <h1 style={{textAlign:"center"}}>Markdown On Steroids</h1>
 
       <Editor text={text} handleChange={handleChange}></Editor>
-
       <Preview preview={preview} />
     </div>
   );
