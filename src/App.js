1f697561871e4a9f5f8191c55a78c4f23b48f1a7
Chirag Lulla <lullachirag239@gmail.coom>
2021-10-21 01:00:34 +0530

    Style all components and set routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import { useState } from "react";
 import marked from "marked";
+import DOMPurify from "dompurify";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Editor from "./Editor";
 import Preview from "./Preview";
+import Header from "./Header";
+import DashBoard from "./DashBoard";
 
-function App() {
+const App = () => {
 
   const initialText = "# This is the biggest text you can ever write.\n## This is the second biggest text you can write.\n###### This is the smallest text you can write.\n\n### **Grab the reader's attention with some Bold text.**\n### *Love for italic text never ends.*\n### ~~Strikethrough the text you wish to.~~\n\n### **You can also _nest_ italic inside bold text.**\n\n### ***Italic and bold at the same time.***\n\nNormal Text\n> Quoted Text\n\n### `Add text which look like real <code></code> as in your editor.`\n\n```\n  const Editor = () => {\n      return 'Convert me to some code looking text.';\n  }\n```\n"
-
   const initialPreview = marked(initialText)
-  
   const [ text, setText ] = useState(initialText)
   const [ preview, setPreview ] = useState(initialPreview)
  
+
+
   const handleChange = e => {
     const text = e.target.value
     setText(text)
-    setPreview(marked(text))
+    const html = marked(text)
+    const sanitizedHtml = DOMPurify.sanitize(html)
+    setPreview(sanitizedHtml)
   }
 
   return (
-    <div className="App">
-      <h1 className="title">Markdown on steroids</h1>
-      <Editor text={text} handleChange={handleChange}></Editor>
-      <Preview preview={preview} />
-    </div>
+    <Router>
+      <div className="App container-fluid my-3">
+        <Header />
+        <div className="row px-4">
+          <Switch>
+            <Route path="/" exact>
+              <Editor text={text} handleChange={handleChange} />
+              <Preview preview={preview} />
+            </Route>
+            <Route path="/dashboard">
+              <DashBoard />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
   
 }
--- src/Editor.js
+++ src/Editor.js
@@ -1,14 +1,19 @@
 const Editor = ({ text, handleChange }) => {
     return ( 
-    <textarea 
-        className="editor"
-        value={text}
-        onChange={handleChange}
-        name="editor" 
-        id="editor"
-    >
-        {text}
-    </textarea>
+        <div className="card col-6 p-0 mh-100 mw-100">
+            <div className="card-header lead">Input</div>
+            <div className="card-body p-0 form-group">
+                <textarea 
+                    className="editor w-100 h-100 form-control border-0"
+                    value={text}
+                    onChange={handleChange}
+                    name="editor" 
+                    id="editor"
+                >
+                    {text}
+                </textarea>
+            </div>
+        </div>
     );
 }
  
