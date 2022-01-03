372a516a0bb72632a8f6f4979f9458346502555f
Chirag Lulla <lullachirag239@gmail.coom>
2022-01-03 21:17:09 +0530

    Fetch documents from database

--- src/Components/DocumentsTable.js
+++ src/Components/DocumentsTable.js
@@ -1,72 +1,109 @@
-import useFetch from "../Hooks/useFetch";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const DocumentsTable = () => {
+  const [markdowns, setMarkdowns] = useState([]);
+  // const {
+  //   data: markdowns,
+  //   isLoading,
+  //   error,
+  // } = useFetch("http://localhost:5000/api/v1/document");
 
-    const { data: markdowns, isLoading, error } = useFetch("http://localhost:8000/markdowns")
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/document", {
+      mode: "cors",
+      credentials: "omit",
+    })
+      .then((res) => {
+        if (!res.ok) throw Error("Cannot fetch data.");
+        return res.json();
+      })
+      .then((markdowns) => {
+        setMarkdowns(markdowns.data.documents);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    let rows;
-    if (markdowns) {
-        rows = markdowns.map(markdown => {
+  const deleteDocument = (id) => {
+    console.log(id);
+    fetch(`http://localhost:5000/api/v1/document/${id}`, {
+      method: "DELETE",
+      headers: { "Access-Control-Request-Method": "DELETE" },
+      mode: "cors",
+      credentials: "omit",
+    })
+      .then((res) => {
+        if (!res.ok) throw Error("Cannot fetch data.");
+        return res.json();
+      })
+      .then((info) => {
+        console.log(info);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="col-12 mt-3 p-0">
+      <h3 className="mx-3">Your Documents</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Document</th>
+            <th></th>
+            <th className="text-center" scope="col">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {markdowns.map((markdown, idx) => {
             return (
-                <tr key={markdown.id}>
-                    <th scope="row">{markdown.id}</th>
-                    <td>{markdown.name}</td>
-                    <td>{markdown.created_at}</td>
-                    <td className="text-center">
-                        <Link
-                            to={`/editor/${markdown.id}`}
-                            className="text-dark mx-3"
-                            styles={{
-                                textDecoration: "none"
-                            }}
-                        >
-                            <i className="fa fa-eye"></i>
-                        </Link>
-                        <Link
-                            to="/"
-                            className="text-dark mx-3"
-                            styles={{
-                                textDecoration: "none"
-                            }}
-                        >
-                            <i className="fa fa-download"></i>
-                        </Link>
-                        <Link
-                            to="/"
-                            className="text-dark mx-3"
-                            styles={{
-                                textDecoration: "none"
-                            }}
-                        >
-                            <i className="fa fa-trash"></i>
-                        </Link>
-                    </td>
-                </tr>
-            )
-        })
-    }
-
-    return (
-        <div className="col-12 mt-3 p-0">
-            <h3 className="mx-3">Your Documents</h3>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Document</th>
-                        <th></th>
-                        <th className="text-center" scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {error && <td>Error</td>}
-                    {isLoading && <td className="text-center">Loading...</td>}
-                    {markdowns && rows}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+              <tr key={markdown._id}>
+                <th scope="row">{idx + 1}</th>
+                <td>{markdown.name}</td>
+                <td>{markdown.createdAt}</td>
+                <td className="text-center">
+                  <Link
+                    to={`/editor/${markdown._id}`}
+                    className="text-dark mx-3"
+                    styles={{
+                      textDecoration: "none",
+                    }}
+                  >
+                    <i className="fa fa-eye"></i>
+                  </Link>
+                  <Link
+                    to="/"
+                    className="text-dark mx-3"
+                    styles={{
+                      textDecoration: "none",
+                    }}
+                  >
+                    <i className="fa fa-download"></i>
+                  </Link>
+                  <Link
+                    to="/"
+                    onClick={(e) => deleteDocument(markdown._id)}
+                    className="text-dark mx-3"
+                    styles={{
+                      textDecoration: "none",
+                    }}
+                  >
+                    <i className="fa fa-trash"></i>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default DocumentsTable;
--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -3,65 +3,65 @@ import { useParams } from "react-router";
 import { useState } from "react";
 import marked from "marked";
 import DOMPurify from "dompurify";
-import Preview from "./Preview"
+import Preview from "./Preview";
 
 const Editor = () => {
+  const { id } = useParams();
 
-    const { id } = useParams()
-    
-    const [text, setText] = useState("")
-    const [preview, setPreview] = useState("")
+  const [text, setText] = useState("");
+  const [preview, setPreview] = useState("");
 
-    useEffect(() => {
-        fetch(`http://localhost:8000/markdowns/${id}`)
-            .then(res => {
-                if(!res.ok)
-                    throw Error("Cannot fetch data.")
-                return res.json()
-            })
-            .then(data => {
-                setText(data.text)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [id])
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/v1/document/${id}`, {
+      mode: "cors",
+      credentials: "omit",
+    })
+      .then((res) => {
+        if (!res.ok) throw Error("Cannot fetch data.");
+        return res.json();
+      })
+      .then((data) => {
+        setText(data.data.document.text);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        const html = marked(text)
-        const sanitizedHtml = DOMPurify.sanitize(html)
-        setPreview(sanitizedHtml)
-    }, [text])
+  useEffect(() => {
+    const html = marked(text);
+    const sanitizedHtml = DOMPurify.sanitize(html);
+    setPreview(sanitizedHtml);
+  }, [text]);
 
-    const handleChange = e => {
-        const text = e.target.value
-        setText(text)
-    }
+  const handleChange = (e) => {
+    const text = e.target.value;
+    setText(text);
+  };
 
-    return (
-
-        <React.Fragment>
-            <div className="card col-6 p-0 full-height">
-                <div className="card-header lead">Input</div>
-                <div className="card-body p-0 form-group">
-                    <textarea
-                        className="editor form-control full-height"
-                        value={text}
-                        onChange={handleChange}
-                        name="editor"
-                        id="editor"
-                        style={{
-                            resize: "none",
-                            borderRadius: 0
-                        }}
-                    >
-                        {text}
-                    </textarea>
-                </div>
-            </div>
-            <Preview preview={preview} />
-        </React.Fragment>
-    );
-}
+  return (
+    <React.Fragment>
+      <div className="card col-6 p-0 full-height">
+        <div className="card-header lead">Input</div>
+        <div className="card-body p-0 form-group">
+          <textarea
+            className="editor form-control full-height"
+            value={text}
+            onChange={handleChange}
+            name="editor"
+            id="editor"
+            style={{
+              resize: "none",
+              borderRadius: 0,
+            }}
+          >
+            {text}
+          </textarea>
+        </div>
+      </div>
+      <Preview preview={preview} />
+    </React.Fragment>
+  );
+};
 
 export default Editor;
